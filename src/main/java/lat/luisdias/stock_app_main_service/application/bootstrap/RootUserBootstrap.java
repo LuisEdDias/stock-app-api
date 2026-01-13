296fdeae65b918ede1751e1522c7409bb8721170
LuisEdDias <edu_vahl@outlook.com>
2026-01-12 21:08:38 -0300

    Update verification to ensure there is only one root user

--- src/main/java/lat/luisdias/stock_app_main_service/application/bootstrap/RootUserBootstrap.java
+++ src/main/java/lat/luisdias/stock_app_main_service/application/bootstrap/RootUserBootstrap.java
@@ -3,8 +3,11 @@ package lat.luisdias.stock_app_main_service.application.bootstrap;
 import lat.luisdias.stock_app_main_service.auth.entities.user.User;
 import lat.luisdias.stock_app_main_service.auth.entities.user.UserRole;
 import lat.luisdias.stock_app_main_service.auth.repositories.user.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +15,10 @@ import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
+@Profile("!test")
 @Component
 public class RootUserBootstrap implements ApplicationRunner {
+    private static final Logger log = LoggerFactory.getLogger(RootUserBootstrap.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final RootSecurityProperties properties;
@@ -31,10 +36,23 @@ public class RootUserBootstrap implements ApplicationRunner {
     @Override
     @Transactional
     public void run(ApplicationArguments args) {
-        if (userRepository.existsByRole(UserRole.ROOT)) {
-            return;
+        int rootUsers = userRepository.countAllByUserRole(UserRole.ROOT);
+        switch (rootUsers) {
+            case 0:
+                validateProperties();
+                createRootUser();
+                log.info("Root user created");
+                log.warn("ROOT user created with credentials from configuration. Change the password immediately.");
+                break;
+            case 1:
+                log.info("Root user found");
+                break;
+            default:
+                throw new IllegalStateException("More than one ROOT user found");
         }
-        validateProperties();
+    }
+
+    private void createRootUser() {
         User rootUser = new User(
                 UUID.randomUUID(),
                 properties.getEmail(),
@@ -42,7 +60,6 @@ public class RootUserBootstrap implements ApplicationRunner {
                 "SUPER USER",
                 UserRole.ROOT
         );
-
         userRepository.save(rootUser);
     }
 
--- src/main/java/lat/luisdias/stock_app_main_service/auth/repositories/user/UserRepository.java
+++ src/main/java/lat/luisdias/stock_app_main_service/auth/repositories/user/UserRepository.java
@@ -1,8 +1,10 @@
 package lat.luisdias.stock_app_main_service.auth.repositories.user;
 
 import lat.luisdias.stock_app_main_service.auth.entities.user.User;
-import lat.luisdias.stock_app_main_service.auth.entities.user.UserRoles;
+import lat.luisdias.stock_app_main_service.auth.entities.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
-    boolean existsByRole(UserRoles role);
+
+    @Query("SELECT COUNT(u.role) FROM User u WHERE u.role = :userRole")
+    int countAllByUserRole(@Param("userRole") UserRole userRole);
 }
