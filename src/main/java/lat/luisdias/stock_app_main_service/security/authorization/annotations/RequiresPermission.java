100d792c1c11f1283606862e808328f8b677e0dc
LuisEdDias <edu_vahl@outlook.com>
2026-01-20 13:17:52 -0300

    Update annotation target

--- src/main/java/lat/luisdias/stock_app_main_service/security/authorization/annotations/RequiresPermission.java
+++ src/main/java/lat/luisdias/stock_app_main_service/security/authorization/annotations/RequiresPermission.java
@@ -2,7 +2,7 @@ package lat.luisdias.stock_app_main_service.security.authorization.annotations;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.METHOD)
+@Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequiresPermission {
     String value();
